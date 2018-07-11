1b71d5d43f0517eb132bab4d8c06d9445aff9a2f
wang <819389372@qq.com>
2018-07-11 16:45:29 +0800

    第二次提交

--- src/main/java/com/wang/User.java
+++ src/main/java/com/wang/User.java
@@ -2,6 +2,15 @@ package com.wang;
 
 public class User {
     private String name;
+    private String age;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 
     public String getName() {
         return name;
